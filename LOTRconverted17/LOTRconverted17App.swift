fdcb2bde35d2d1b7122150043f31300b44885fc0
harry <revolutiongill@gmail.com>
2024-05-14 13:34:18 +0530

    Initial Commit

---
+++ LOTRconverted17/LOTRconverted17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRconverted17App.swift
+//  LOTRconverted17
+//
+//  Created by Harry Gill on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRconverted17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
